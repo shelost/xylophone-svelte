a89331cd9dfe0749a4ef41c6d63cd2b4d69b3e57
Heewon Ahn <ha268@cornell.edu>
2023-07-27 00:42:11 +0900

    stage changes

---
+++ src/routes/(app)/dashboard/space/[slug]/canvas/[slug]/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import { supabaseClient } from '$lib/db';
+    import { onMount } from 'svelte';
+    import { writable } from 'svelte/store';
+    //import DrawingCanvas from '$lib/components/DrawingCanvas.svelte'
+
+    export let data;
+</script>
+
+<h1> Hello </h1>
