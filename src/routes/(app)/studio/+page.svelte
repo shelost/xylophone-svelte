b3c4fe15451ed13b0fa9fc90304889a5a11b09c3
Heewon Ahn <ha268@cornell.edu>
2023-08-28 14:22:48 -0400

    upgrades, people, upgrades

--- src/routes/(app)/studio/+page.svelte
+++ src/routes/(app)/studio/+page.svelte
@@ -1,27 +1,35 @@
-<div id = 'buttons'>
-    <button id="addText">Add Text</button>
-    <button id="addImage">Add Image</button>
-    <button id="addVideo">Add Video</button>
-    <button id="downloadJSON">Download </button>
-</div>
+<div id = 'app'>
+
+    <div id="floatingOptions">
+        <button id="addTextOption">Add Text</button>
+        <button id="addImageOption">Add Image</button>
+        <button id="addVideoOption">Add Video</button>
+    </div>
 
 
 
-<div id="floatingOptions">
-    <button id="addTextOption">Add Text</button>
-    <button id="addImageOption">Add Image</button>
-    <button id="addVideoOption">Add Video</button>
-</div>
+    <div id = 'container'>
+        <canvas id = 'canvas'></canvas>
+    </div>
 
 
+    <div id = 'panel'>
 
-<div id = 'container'>
-<canvas id = 'canvas'></canvas>
-</div>
+        <div id = 'buttons'>
+            <button id="addText">Add Text</button>
+            <button id="addImage">Add Image</button>
+            <button id="addVideo">Add Video</button>
+            <button id="downloadJSON">Download </button>
+        </div>
 
 
-<div id = 'controls'>
-<div id = 'title'> Text </div>
+        <div id = 'title'> Text </div>
+
+        <div id = 'controls'>
+
+        </div>
+    </div>
+
 </div>
 
 
@@ -29,9 +37,25 @@
 
 <style lang='scss'>
 
+    ::-webkit-scrollbar{
+        width: 3px;
+        height: 0;
+        background: white;
+    }
+
+    ::-webkit-scrollbar-thumb{
+        background: rgba(black, 0.4);
+    }
+
+    #app{
+        width: calc(100vw - 240px);
+        height: 100vh;
+        overflow: hidden !important;
+    }
+
     #canvas{
         height: 100vh;
-        width: calc(100vw - 400px);
+        width: calc(100vw - 480px);
         background: white;
     }
 
@@ -40,20 +64,63 @@
         opacity: 0;
     }
 
-    #buttons{
+
+    #panel{
+        display: flex;
+        flex-direction: column;
         position: fixed;
-        top: 20px;
-        right: 260px;
-        z-index: 3;
+        top: 0;
+        right: 0;
+        height: 100vh;
+        width: 240px;
+        background: rgba(black, 0.05);
+
+        #buttons{
+            gap: 20px;
+            margin: 30px;
+        }
+
+        #controls{
+            label{
+                font-size: 12px !important;
+                display: flex;
+                flex-direction: column;
+            }
+            #fontFamily{
+                background: white;
+            }
+            input{
+                background: white;
+                padding: 5px;
+            }
+        }
+
+
     }
 
     .active{
         opacity: 1;
     }
 
+    #container {
+        flex-grow: 1;
+        width: calc(100vw - 480px);
+        height: 100vh;
+        display: flex;
+        justify-content: flex-start;
+        align-items: flex-start;
+        overflow-x: hidden;
+
+        overflow-y: scroll;
+
+
+    }
+
+    #canvas {
+        flex-shrink: 0;
+    }
+
 </style>
-
-
 
 
 
@@ -66,15 +133,68 @@ import {fabric} from 'fabric'
 
 onMount(()=> {
     // Initialize fabric.js canvas
+
+    let initialCanvasWidth = window.innerWidth - 480;  // Initial canvas width, 250 is the panel width
+let initialCanvasHeight = window.innerHeight;  // Initial canvas height
+
+
+
 const canvas = new fabric.Canvas('canvas', {
-  width: window.innerWidth - 400,
-  height: window.innerHeight
+  width: initialCanvasWidth,
+  height: initialCanvasHeight,
+  // ...other options
 });
 
-canvas.setDimensions({width: window.innerWidth - 400, height:  window.innerHeight}, {cssOnly: true});
 
-canvas.renderAll()
-canvas.calcOffset();
+
+    canvas.renderAll()
+    canvas.calcOffset();
+
+
+    window.onresize = debounce(() => {
+    const panelWidth = 250;
+    const newWidth = window.innerWidth - panelWidth;
+    const scaleX = newWidth / initialCanvasWidth;
+
+    // Scale each object
+    canvas.forEachObject((object) => {
+        object.left *= scaleX;
+        object.scaleX *= scaleX;
+        object.top *= scaleX;
+        object.scaleY *= scaleX;
+        object.setCoords();
+    });
+
+    // Update canvas dimensions
+    canvas.setWidth(newWidth);
+    canvas.setHeight(initialCanvasHeight * scaleX);
+    canvas.renderAll();
+
+    // Update the bottom position to extend canvas if required
+    let bottomMost = 0;
+    canvas.forEachObject((object) => {
+        const bottom = object.top + object.height * object.scaleY;
+        if (bottom > bottomMost) {
+            bottomMost = bottom;
+        }
+    });
+
+    calculateGrid()
+    drawGrid()
+
+    if (bottomMost > canvas.getHeight()) {
+        canvas.setHeight(bottomMost);
+    }
+
+    // Update initial dimensions
+    initialCanvasWidth = newWidth;
+    initialCanvasHeight = canvas.getHeight();
+
+
+    // Re-render the canvas
+    canvas.renderAll();
+});
+
 
 let initialX, initialY;
 
@@ -105,6 +225,16 @@ function addImage(x,y) {
     });
     canvas.add(image);
   });
+}
+
+function debounce(func, wait) {
+  let timeout;
+  return function () {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      func.apply(this, arguments);
+    }, wait);
+  };
 }
 
 // Function to add video
@@ -191,44 +321,87 @@ canvas.on('text:editing:entered', (textObject) => {
 /////
 
 
-const xArr = Array.from({length: 40}, (_, i) => i * 40);
-const yArr = Array.from({length: 40}, (_, i) => i * 40);
+let xArr = Array.from({length: 40}, (_, i) => i * 40);
+let yArr = Array.from({length: 40}, (_, i) => i * 40);
+let gridLines = [];
 
-// Draw the grid on the canvas
-xArr.forEach(x => {
-  canvas.add(new fabric.Path(`M ${x}, 0 V ${canvas.height}`, {
-    stroke: '#eee',
-    selectable: false
-  }));
-});
-yArr.forEach(y => {
-  canvas.add(new fabric.Path(`M 0, ${y} H ${canvas.width}`, {
-    stroke: '#eee',
-    selectable: false
-  }));
-});
 
-// Snapping function when object is moving
-canvas.on('object:moving', function(event) {
-  const obj = event.target;
-  const closestX = findClosest(obj.left, xArr);
-  const closestY = findClosest(obj.top, yArr);
-  obj.set({
-    left: closestX,
-    top: closestY
-  }).setCoords();  // Update object's coordinates
-});
 
-// Find the closest value in an array to a given number
-function findClosest(num, arr) {
-  return arr.reduce((prev, curr) =>
-    Math.abs(curr - num) < Math.abs(prev - num) ? curr : prev
-  );
+function removeGrid() {
+    gridLines.forEach(line => {
+        canvas.remove(line);
+    });
+    gridLines = [];
+}
+
+function calculateGrid() {
+    xArr = [];
+    yArr = [];
+
+    const canvasWidth = canvas.getWidth();
+    const canvasHeight = canvas.getHeight();
+
+    const columns = 40;  // The number of columns you want.
+    const gridSpacing = canvasWidth / columns; // Now gridSpacing is dynamic.
+
+    for(let x = 0; x <= canvasWidth; x += gridSpacing) {
+        xArr.push(x);
+    }
+
+    // Assuming you want to keep the aspect ratio the same for rows as well.
+    for(let y = 0; y <= canvasHeight; y += gridSpacing) {
+        yArr.push(y);
+    }
+}
+
+function drawGrid(){
+    // Draw the grid on the canvas
+
+    removeGrid();
+
+
+     // Draw new grid
+     xArr.forEach(x => {
+        const line = new fabric.Path(`M ${x}, 0 V ${canvas.height}`, {
+            stroke: '#eee',
+            selectable: false
+        });
+        canvas.add(line);
+        canvas.sendToBack(line);
+        gridLines.push(line); // Populate gridLines array
+    });
+
+    yArr.forEach(y => {
+        const line = new fabric.Path(`M 0, ${y} H ${canvas.width}`, {
+            stroke: '#eee',
+            selectable: false
+        });
+        canvas.add(line);
+        canvas.sendToBack(line);
+        gridLines.push(line); // Populate gridLines array
+    });
+
+    // Snapping function when object is moving
+    canvas.on('object:moving', function(event) {
+    const obj = event.target;
+    const closestX = findClosest(obj.left, xArr);
+    const closestY = findClosest(obj.top, yArr);
+    obj.set({
+        left: closestX,
+        top: closestY
+    }).setCoords();  // Update object's coordinates
+    });
+
+    // Find the closest value in an array to a given number
+    function findClosest(num, arr) {
+        return arr.reduce((prev, curr) =>
+            Math.abs(curr - num) < Math.abs(prev - num) ? curr : prev
+        );
+    }
 }
 
 
-////
-
+drawGrid()
 
 
 
@@ -242,18 +415,55 @@ function textTemplate(activeObject) {
   return options;
 }
 
+function imageTemplate(activeObject) {
+  return [
+    {
+      label: 'Width',
+      id: 'scaleX',
+      type: 'number',
+      value: activeObject.width * activeObject.scaleX,
+    },
+    {
+      label: 'Height',
+      id: 'scaleY',
+      type: 'number',
+      value: activeObject.height * activeObject.scaleY,
+    },
+    {
+      label: 'Filter',
+      id: 'filter',
+      type: 'select',
+      value: 'normal', // Initial value
+      options: ['normal', 'invert', 'monochrome'],
+    }
+  ];
+}
+
+function videoTemplate(activeObject) {
+  const options = [
+    { label: 'Autoplay', id: 'autoplay', type: 'checkbox', value: activeObject.autoplay || false },
+    { label: 'Controls', id: 'controls', type: 'checkbox', value: activeObject.controls || false },
+    { label: 'Width', id: 'width', type: 'number', value: activeObject.width || 0 },
+    { label: 'Height', id: 'height', type: 'number', value: activeObject.height || 0 }
+  ];
+  return options;
+}
 
 function populatePanel(activeObject) {
   let panel = document.getElementById('controls');
   panel.innerHTML = ''; // Clear existing options
-
   let options = [];
 
   switch (activeObject.type) {
     case 'i-text':
       options = textTemplate(activeObject);
       break;
-    // Future cases for other types like 'image', 'video'
+    case 'image':
+      options = imageTemplate(activeObject);
+      break;
+    case 'video':
+      options = videoTemplate(activeObject);
+      break;
     default:
       break;
   }
@@ -272,11 +482,39 @@ function populatePanel(activeObject) {
 
     // Attach event listener
     input.addEventListener('change', function() {
-      activeObject.set({ [option.id]: input.type === 'number' ? parseFloat(input.value) : input.value });
+      if (input.type === 'checkbox') {
+        activeObject.set({ [option.id]: input.checked });
+      } else {
+        activeObject.set({ [option.id]: input.type === 'number' ? parseFloat(input.value) : input.value });
+      }
       canvas.renderAll();
     });
   });
 }
+
+
+
+function applyFilter(image, filterName) {
+  switch (filterName) {
+    case 'normal':
+      image.filters = [];
+      break;
+    case 'invert':
+      image.filters = [new fabric.Image.filters.Invert()];
+      break;
+    case 'monochrome':
+      image.filters = [new fabric.Image.filters.BlackWhite()];
+      break;
+    default:
+      break;
+  }
+  image.applyFilters();
+  canvas.renderAll();
+}
+
+
+
+
 
 
 canvas.on('selection:created', function(event) {
