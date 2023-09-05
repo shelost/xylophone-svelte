289eb3c3f28b3172c388de870f973dffb3edc000
Heewon Ahn <ha268@cornell.edu>
2023-09-05 11:53:14 -0400

    fixed env

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/auth-helpers-sveltekit';
 import type { Database } from '../../DatabaseDefinitions'
 
 export const supabaseClient = createClient(
-  'https://daiyycuunubdakrxtztl.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRhaXl5Y3V1bnViZGFrcnh0enRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTMxMDQ2MzMsImV4cCI6MjAwODY4MDYzM30.JHOlKkaa3XpZZGsDpyjYcHitCiO1aNLaINnAnm0KHuQ'
+  import.meta.env.VITE_PUBLIC_SUPABASE_URL,
+  import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY,
 );
